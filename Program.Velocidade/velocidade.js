c3de86e38e1c5d854846d06abbbe2fec544ab4a2
Weslley157 <113360895+Weslley157@users.noreply.github.com>
2023-10-16 07:55:33 -0300

    Add files via upload

--- Program.Velocidade/velocidade.js
+++ Program.Velocidade/velocidade.js
@@ -1,21 +1,17 @@
-// Obtenha as referências dos elementos de entrada no formulário
+
 const velocidadePermitidaInput = document.getElementById('velocidadePermitida');
 const velocidadeCondutorInput = document.getElementById('velocidadeCondutor');
 const resultadoOutput = document.querySelector('h3');
 
-// Adicione um ouvinte de evento para o formulário ser enviado
 document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Impede o envio padrão do formulário
+    e.preventDefault(); 
 
-    // Obtém os valores de velocidade do formulário
     const velocidadePermitida = parseFloat(velocidadePermitidaInput.value);
     const velocidadeCondutor = parseFloat(velocidadeCondutorInput.value);
 
-    
-    // Verifique a velocidade do condutor em relação à velocidade permitida
     if (velocidadeCondutor <= velocidadePermitida) {
         resultadoOutput.textContent = 'Dentro do limite de velocidade. Dirija com segurança!';
-    } else {
+    } else{
         resultadoOutput.textContent = 'Você está acima do limite de velocidade. Tenha cuidado!';
     }
 });
